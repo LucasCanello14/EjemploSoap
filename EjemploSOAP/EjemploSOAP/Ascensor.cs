ae3fd467b74791bb820ab0abc7c3439338533443
Lucas Canello <Lucas.canello1404@gmail.com>
2023-05-31 21:53:45 -0300

    inicio logica negocio

---
+++ EjemploSOAP/EjemploSOAP/Ascensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploSOAP
+{
+    internal class Ascensor
+    {
+        public int Codigo { get; set; }
+        public int PisoActual { get; set; }
+        public bool Ocupado { get; set; }
+    }
+}
---
+++ EjemploSOAP/EjemploSOAP/Edificio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploSOAP
+{
+    internal class Edificio
+    {
+        public List<Ascensor> ascensores;
+        public Edificio()
+        {
+            ascensores = new List<Ascensor>();  
+        }
+        
+        public bool EstadoAscensor(int codigo)
+        {
+            bool ocupado;
+            Ascensor ascensor = ascensores.First(x => x.Codigo == codigo);
+            if (ascensor.Ocupado)
+            {
+                ocupado = true;
+            }
+            else
+            {
+                ocupado = false;
+            }
+
+            return ocupado;
+        }
+
+        public int SolicitarAscensor(int nroPiso, int codigo)
+        {
+            Ascensor ascensor = ascensores.First(x => x.Codigo == codigo);
+            if (!ascensor.Ocupado)
+            {
+                ascensor.Ocupado = true;
+                int pisoInicial = ascensor.PisoActual;
+            }
+            return ;
+        }
+    }
+}
